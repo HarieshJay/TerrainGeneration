e9cb8cf80503d0de542cb94e409d3969e29f8aa3
Hariesh <jayanthh@mcmaster.ca>
2019-11-26 10:40:46 -0500

    plane

--- world.cpp
+++ world.cpp
@@ -21,8 +21,8 @@ int gWidth = 400;
 int cnt = 0;
 int ang = 0;
 bool isPaused = false;
-float eye[3] = {20,20,20};
-const int planeSize = 300;
+float eye[3] = {800,800,10};
+const int planeSize = 800;
 int numOfMountains = 20;
 vector<int[2]> mountains = vector<int[2]>();
 float heightMap[planeSize][planeSize];
@@ -49,8 +49,8 @@ void intializeHeightMap(){
 
 void circleAlgo(int x, int y){
 
-    int radius = static_cast <int> (rand()) % 20 + 1;
-    float disp = static_cast <int> (rand()) % 12 + 1;
+    int radius = static_cast <int> (rand()) % 100 + 1;
+    float disp = static_cast <int> (rand()) % 100 + 100;
 
     for ( int i = 0; i < planeSize; i++){
         for ( int j = 0; j < planeSize; j++){
@@ -97,11 +97,27 @@ void update(){
 
 
 void createPlane(){
-    glPushMatrix();
-    glBegin(GL_QUADS);
+    glColor3f(0,1,1);
+
     
-    glEnd();
-    glPopMatrix();
+    for ( int a = 0; a < planeSize; a++){
+    
+        for ( int b = 0; b < planeSize; b++){
+
+            glBegin(GL_QUADS);
+            if ( a+1 < planeSize && b+1 < planeSize){
+
+                glVertex3f(a, b , heightMap[a][b]);
+                glVertex3f(a+1, b, heightMap[a+1][b]);
+                glVertex3f(a+1, b+1, heightMap[a+1][b+1]);
+                glVertex3f(a, b+1, heightMap[a][b+1]);
+                
+            }
+            glEnd();
+
+        }
+    }
+    
 }
 
 
@@ -129,14 +145,14 @@ void draw3DScene(){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(eye[0], eye[1], eye[2], 0, 8, 0, 0, 1, 0);
+    gluLookAt(eye[0], eye[1], eye[2], 0, 8, 0, 0, 0, 1);
     
 
-    fillHeightMap();
+    createPlane();
    
     cnt++;
 
-    createPlane();
+    glColor3f(1,0,0);
     
     ang++;
    
@@ -184,53 +200,56 @@ void SpecialInput(int key, int x, int y)
 
 int main(int argc, char** argv)
 {
-    printf("\n"
-        "r -> reset\n"
-        "q -> quit\n"
-        "space -> stop simulation\n"
-        "l -> toggle lights\n"
-        "f -> add friction\n"
-        "p -> pause simulation\n"
-        "r -> reset\n"
-        "up arrow -> move camera up\n"
-        "down arrow -> move camera down\n"
-        "left arrow -> move camera left\n"
-        "right arrow -> move camera right\n"
-        "LIGHT1:\n"
-        "1 -> move x in positive\n"
-        "2 -> move x in negative\n"
-        "3 -> move y in positive\n"
-        "4 -> move y in negative\n"
-        "5 -> move z in positive\n"
-        "6 -> move z in negative\n"
-        "LIGHT2:\n"
-        "z -> move x in positive\n"
-        "x -> move x in negative\n"
-        "c -> move y in positive\n"
-        "v -> move y in negative\n"
-        "b -> move z in positive\n"
-        "n -> move z in negative\n"
-        "ADDITION CAPABILITIES:\n"
-        "m -> follow a particle\n"
-        "t -> add various types of particles and momentums\n");
+    // printf("\n"
+    //     "r -> reset\n"
+    //     "q -> quit\n"
+    //     "space -> stop simulation\n"
+    //     "l -> toggle lights\n"
+    //     "f -> add friction\n"
+    //     "p -> pause simulation\n"
+    //     "r -> reset\n"
+    //     "up arrow -> move camera up\n"
+    //     "down arrow -> move camera down\n"
+    //     "left arrow -> move camera left\n"
+    //     "right arrow -> move camera right\n"
+    //     "LIGHT1:\n"
+    //     "1 -> move x in positive\n"
+    //     "2 -> move x in negative\n"
+    //     "3 -> move y in positive\n"
+    //     "4 -> move y in negative\n"
+    //     "5 -> move z in positive\n"
+    //     "6 -> move z in negative\n"
+    //     "LIGHT2:\n"
+    //     "z -> move x in positive\n"
+    //     "x -> move x in negative\n"
+    //     "c -> move y in positive\n"
+    //     "v -> move y in negative\n"
+    //     "b -> move z in positive\n"
+    //     "n -> move z in negative\n"
+    //     "ADDITION CAPABILITIES:\n"
+    //     "m -> follow a particle\n"
+    //     "t -> add various types of particles and momentums\n");
 
 
     fillHeightMap();
-    for (int i = 0; i < planeSize; ++i)
-    {
-        for (int j = 0; j < planeSize; ++j)
-        {
-            std::cout << heightMap[i][j] << ' ';
-        }
-        std::cout << std::endl;
-    }
+    // for (int i = 0; i < planeSize; ++i)
+    // {
+    //     for (int j = 0; j < planeSize; ++j)
+    //     {
+    //         std::cout << heightMap[i][j] << ' ';
+    //     }
+    //     std::cout << std::endl;
+    // }
 	
 	glutInit(&argc, argv);
 	glutInitWindowSize(800, 800);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-	glutCreateWindow("Assignment 3");
+	glutCreateWindow("Assignment 4");
 	glutTimerFunc(0, FPS, 0);
    
+
+    fillHeightMap();
+
     glEnable(GL_COLOR_MATERIAL);
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
